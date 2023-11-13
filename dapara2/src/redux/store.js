0dfd085750210fdfdbd14c6ec650621baadaf10d
thdwlgus <jiniluce7@naver.com>
2023-11-13 15:56:53 +0900

    1st

--- dapara2/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartSliceReducer from './slices/cartSlice'
-
-export const store = configureStore({
-  reducer: {
-    cart: cartSliceReducer,
-  },
-  devTools: process.env.MODE_ENV !== 'production',
-})
